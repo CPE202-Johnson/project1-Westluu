11787017198e95827da92d20fae318f392d19374
Wesley <wluu03@calpoly.edu>
2021-01-16 18:24:25 -0800

    Added more test cases

--- perm_lex_testcases.py
+++ perm_lex_testcases.py
@@ -2,9 +2,25 @@ import unittest
 import perm_lex
 
 class TestAssign1(unittest.TestCase):
+    """ This test if the function is able to take a spring and
+        output all the possiable peermutation of that spring in
+        lexicographic order.
+    """
 
     def test_perm_gen_lex(self):
         self.assertEqual(perm_lex.perm_gen_lex('ab'),['ab','ba'])
+
+        self.assertEqual(perm_lex.perm_gen_lex('abc'), ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+
+        self.assertEqual(perm_lex.perm_gen_lex('def'), ['def', 'dfe', 'edf', 'efd', 'fde', 'fed'])
+
+        self.assertEqual(perm_lex.perm_gen_lex('abcd'), ['abcd', 'abdc', 'acbd', 'acdb', 'adbc',
+                                                         'adcb', 'bacd', 'badc', 'bcad', 'bcda', 
+                                                         'bdac', 'bdca', 'cabd', 'cadb', 'cbad', 
+                                                         'cbda', 'cdab', 'cdba', 'dabc', 'dacb',
+                                                         'dbac', 'dbca', 'dcab', 'dcba'])
+        
+                             
 
 if __name__ == "__main__":
         unittest.main()
